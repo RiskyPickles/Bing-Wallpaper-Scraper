d01f52ee705f78cc99d6635b76eeb16a110c0d45
RiskyPickles <lafeid.314@gmail.com>
2020-01-27 18:09:13 -0500

    Added function to auto-generate the archive urls to download from.

--- scaper.py
+++ scaper.py
@@ -3,79 +3,82 @@ import os
 import urllib.request
 from bs4 import BeautifulSoup
 
-wallpage1 = "https://bing.gifposter.com/archive.html"
-wallpage2 = "https://bing.gifposter.com/archive/201912.html"
+def manage_sources():
+	wallpage1 = "https://bing.gifposter.com/archive.html"
+	wallpage2 = "https://bing.gifposter.com/archive/201912.html"
 
-months_2019 = []
-months_2018 = []
+	months_2019 = []
+	months_2018 = []
 
-wallpaper_page = wallpage2
-result = requests.get(wallpaper_page)
+	for i in range(1,13):
+		if i < 10:
+			months_2018.append("20180" + str(i))
+			months_2019.append("20190" + str(i))
+		else:
+			months_2018.append("2018" + str(i))
+			months_2019.append("2019" + str(i))
+		
+	# ~ print (months_2018)
+	# ~ print (months_2019)
 
-if result.status_code == 200:
-	soup = BeautifulSoup(result.content, "html.parser")
-
-# ~ print (soup)
-# ~ for tag in soup.find_all("a"):
-	# ~ urls.append(tag['src'])
-
-urls = []
-for tag in soup.find_all("img"):
-	urls.append(tag['src'])
+	urls_2019 = []
+	urls_2018 = []
 	
-urls.pop()
-# ~ print (urls)
+	def generate_dirs_and_urls(month_list, url_list):
+		for month in month_list:
+			## Make new directories for each month
+			dir_path = os.path.join(os.getcwd(), month)
+			print (dir_path)
+			# ~ os.mkdir(dir_path)
+			
+			## Generate URLs for each archive download source.
+			base = "https://bing.gifposter.com/archive"
+			url_list.append(base + "/" + month + ".html")
+	
+	generate_dirs_and_urls(months_2018, urls_2018)
+	generate_dirs_and_urls(months_2019, urls_2019)
+	
+	## Checking I didn't fuck anything up in the naming of urls
+	print (urls_2018)
+	print (urls_2019)
+	
 
-for count, url in enumerate(urls):
-	urls[count] = url[:-3] # remove _sm from url for full size image
-	name = urls[count][36:] # cut filename from domain
+def download():
+	wallpaper_page = wallpage2
+	result = requests.get(wallpaper_page)
 
-	print ("...Downloading " + name + " from " + urls[count])
-	urllib.request.urlretrieve(
-		urls[count], os.path.join(os.getcwd(), name))
-"""
-def sample():
-	# import the necessary packages
-	from bs4 import BeautifulSoup as bs
-	import requests
-	import urllib.request
-	import os
-	import argparse
+	if result.status_code == 200:
+		soup = BeautifulSoup(result.content, "html.parser")
 
-	#Define the argument parser to read in the URL
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-fname', '--fname', help='CSV of image links')
-	args = vars(parser.parse_args())
-	f_name = args['fname']
+	# ~ print (soup)
+	# ~ for tag in soup.find_all("a"):
+		# ~ urls.append(tag['src'])
 
-	# Create the directory name where the images will be saved
-	base_dir = os.getcwd()
-	dir_name = (f_name.split('/')[-1]).split('.')[0]
-	dir_path = os.path.join(base_dir, dir_name)
+	urls = []
+	for tag in soup.find_all("img"):
+		urls.append(tag['src'])
+		
+	urls.pop()
+	# ~ print (urls)
 
-	#Create the directory if already not there
-	if not os.path.exists(dir_path):
-		os.mkdir(dir_path)
+	for count, url in enumerate(urls):
+		urls[count] = url[:-3] # remove _sm from url for full size image
+		name = urls[count][36:] # cut filename from domain
 
-	# Read the csv with links to all the image pages
-	f = open(os.path.join(base_dir, f_name),'r')
-	links = f.read().split(',')
+		print ("...Downloading " + name + " from " + urls[count])
+		urllib.request.urlretrieve(
+			urls[count], os.path.join(os.getcwd(), name))
 
-	# Print the number of images
-	print ("[INFO] Downloading {} images".format(len(links)))
-
-	# Function to take an image url and save the image in the given directory
-	def download_image(url):
-		print("[INFO] downloading {}".format(url))
-		name = str(url.split('/')[-1])
-		urllib.request.urlretrieve(url,os.path.join(dir_path, name))
-
-	for href in links:
-		# Extract the contents of the link
-		img_page = requests.get(href)
-		soup = bs(img_page.content, 'html.parser')
-		img_class = soup.find_all('meta', attrs={"name":"twitter:image"})
-		img_url = img_class[0].get('content')
-		real_url = img_url.replace('XL', 'X3', 2)
-		download_image(real_url)
-"""
+def main():
+	manage_sources()
+	
+main()
+	
+	
+	
+	
+	
+	
+	
+	
+	
